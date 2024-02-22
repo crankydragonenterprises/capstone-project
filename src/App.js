f6e780b490aa39b884fc2233ec6444ca9dde62ba
Angela Tarbett <angelatarbett@tobiidynavox.com>
2024-02-22 09:30:37 -0500

    Navigation Component Work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'; 
 import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 import Navigation from './routes/navigation/navigation.component';
 
@@ -18,6 +19,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return (
+        <div>
+            <h1>This is the Sign In Page</h1>
+        </div>
+    )
+}
+
+export default SignIn;
