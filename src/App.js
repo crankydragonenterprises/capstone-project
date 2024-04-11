12b11559381c6b80c4a935704045aacd930c369f
Angela Tarbett <angelatarbett@tobiidynavox.com>
2024-04-11 09:29:53 -0400

    converted user and category contexts to reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import { Routes, Route } from 'react-router-dom'; 
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import { setCurrentUser } from './store/user/user.action';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
@@ -6,6 +11,20 @@ import Shop from './routes/shop/shop.component';
 import CheckoutPage from './routes/checkout/checkout.component';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if(user)
+        {
+            createUserDocumentFromAuth(user);
+        }
+        dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
